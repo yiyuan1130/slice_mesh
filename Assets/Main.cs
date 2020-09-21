31a0236a88b977ca0c68720f7461ff8bb5ac7123
liyiyuan@ihuman.com <liyiyuan@ihuman.com>
2020-09-21 15:32:06 +0800

    get need slide points

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -9,9 +9,10 @@ public class Main : MonoBehaviour {
 	public LineRenderer lineRenderer;
 
 	public GameObject testPoint;
+	public Mesh mesh;
 
 	void Start () {
-		
+		mesh = meshObject.GetComponent<MeshFilter>().mesh;
 	}
 	
 	Vector3 downPos = Vector3.one * 1000;
@@ -28,8 +29,8 @@ public class Main : MonoBehaviour {
 		}
 		if (drawing) {
 			upPos = MousePostionToWorld(Input.mousePosition, meshObject.transform);
-			lineRenderer.startWidth = 0.1f;
-			lineRenderer.endWidth = 0.1f;
+			lineRenderer.startWidth = 0.05f;
+			lineRenderer.endWidth = 0.05f;
 			lineRenderer.SetPositions(new Vector3[]{
 				downPos, upPos
 			});
@@ -46,8 +47,10 @@ public class Main : MonoBehaviour {
 		// Debug.Log("downVertics " + downVertics.Count);
 		// Debug.Log("onVertices " + onVertices.Count);
 		DebugClearPoint();
-		DebugPoint(upVertices, Color.red);
-		DebugPoint(downVertics, Color.green);
+		DebugPoint(upVertices, Color.blue, 0.019f);
+		DebugPoint(downVertics, Color.green, 0.019f);
+		DebugPoint(onVertices, Color.yellow, 0.019f);
+		GetLinePanelTriangles();
 	}
 
 	void DrawPanel(){
@@ -62,7 +65,7 @@ public class Main : MonoBehaviour {
         return Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, normardir.magnitude));
     }
 
-	// 顶点分类成面上面下
+	// 1. 顶点分类成面上面下
 	void SplitVerticesByPanel(out List<Vector3> upVertices, out List<Vector3> downVertics, out List<Vector3> onVertics){
 		// 面的法线
 		Vector3 panelNormalVec = panel.transform.up;
@@ -70,7 +73,7 @@ public class Main : MonoBehaviour {
 		// 面上任意一点
 		Vector3 panelCenterPos = panel.transform.position;
 
-		Mesh mesh = meshObject.GetComponent<MeshFilter>().mesh;
+		// Mesh mesh = meshObject.GetComponent<MeshFilter>().mesh;
 		upVertices = new List<Vector3>();
 		downVertics = new List<Vector3>();
 		onVertics = new List<Vector3>();
@@ -97,6 +100,52 @@ public class Main : MonoBehaviour {
 		return obj.transform.TransformPoint(posInObj);
 	}
 
+	// 2. 计算与面相交的三角形
+	// 三角形的三个点不在面的同一侧
+	void GetLinePanelTriangles(){
+		Vector3 panelNormalVec = panel.transform.up;
+		Vector3 panelCenterPos = panel.transform.position;
+
+		int[] triangles = mesh.triangles;
+		List<int[]> tris = new List<int[]>();
+		for (int i = 0; i < triangles.Length - 3; i+=3)
+		{
+			float upCount = 0;
+			float downCount = 0;
+			for (int j = 0; j < 3; j++)
+			{
+				Vector3 vert = mesh.vertices[triangles[i + j]];	
+				Vector3 worldPos = ObjectPosToWorldPos(vert, meshObject);
+				float dis = CaculatePos(panelNormalVec, panelCenterPos, worldPos);
+				if (dis < 0){
+					downCount ++;
+				}
+				else{
+					upCount ++;
+				}
+			}
+			if ((upCount == 1 && downCount == 2) || (upCount == 2 && downCount == 1)){
+				tris.Add(new int[]{
+					triangles[i + 0],
+					triangles[i + 1],
+					triangles[i + 2],
+				});
+			}
+		}
+
+		Debug.Log("++++++++ " + tris.Count);
+
+
+		// debug
+		for (int i = 0; i < tris.Count; i++)
+		{
+			for (int j = 0; j < tris[i].Length; j++)
+			{
+				Debug.Log(tris[i][j]);
+				DebugPoint(mesh.vertices[tris[i][j]], Color.red, 0.02f);
+			}
+		}
+	}
 
 	List<GameObject> debugPoints = new List<GameObject>();
 	void DebugClearPoint(){
@@ -105,15 +154,28 @@ public class Main : MonoBehaviour {
 			GameObject.Destroy(item);
 		}
 	}
-	void DebugPoint(List<Vector3> points, Color color){
-
+	void DebugPoint(List<Vector3> points, Color? color = null, float? scale = null){
+		color = color != null ? color : Color.red;
+		scale = scale != null ? scale : 0.05f;
 		for (int i = 0; i < points.Count; i++)
 		{
 			GameObject go = GameObject.Instantiate(testPoint);
 			go.SetActive(true);
 			debugPoints.Add(go);
+			go.transform.localScale = Vector3.one * (float)scale;
 			go.transform.position = ObjectPosToWorldPos(points[i], meshObject);
-			go.GetComponent<MeshRenderer>().material.SetColor("_Color", color);
+			go.GetComponent<MeshRenderer>().material.SetColor("_Color", (Color)color);
 		}
+	}
+
+	void DebugPoint(Vector3 point, Color? color = null, float? scale = null){
+		color = color != null ? color : Color.red;
+		scale = scale != null ? scale : 0.05f;
+		GameObject go = GameObject.Instantiate(testPoint);
+		go.SetActive(true);
+		debugPoints.Add(go);
+		go.transform.localScale = Vector3.one * (float)scale;
+		go.transform.position = ObjectPosToWorldPos(point, meshObject);
+		go.GetComponent<MeshRenderer>().material.SetColor("_Color", (Color)color);
 	}
 }
